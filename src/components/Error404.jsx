e3f13a6d51654987ddc004b59bc5a8119a75a11a
Kristen Chellis and Matt Miller <matt@>
2018-06-27 16:19:13 -0700

    Add cute puppy pic to error.

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import tenor from '../assets/images/tenor.png';
 
 function Error404(props){
   return (
     <div>
+      <img src={tenor}/>
       <h2>The page {props.location.pathname} does not exist!</h2>
       <h3>Would you like to return <Link to="/">home</Link> instead?</h3>
     </div>
